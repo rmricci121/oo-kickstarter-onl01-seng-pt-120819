c5a445e0a12521107e9ff1154f3a5c2e5ad8c3ea
Robert M Ricci <rmricci121@gmail.com>
2020-01-18 01:22:14 +0000

    Automatically backed up by Learn

--- lib/backer.rb
+++ lib/backer.rb
@@ -9,8 +9,8 @@ class Backer
     @backed_projects = []
   end
   
-  def back_project(title)
-    @backed_projects << Project.new(title)
+  def back_project(project)
+    @backed_projects << Project.new(project)
      
     #binding.pry
   end
